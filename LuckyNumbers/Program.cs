4726bac364f5d17240c01d224dec0e7714a4dc9c
jsales3 <jsales3>
2018-01-29 09:14:56 -0500

    final commit

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,24 +10,30 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            //decalring variables
             int startNum;
             int endNum;
             int[] userNum = new int[6];
-            int correctNumbers = 0;
             string answer = "yes";
             double winings;
             double jackpot = 50000;
 
             do
             {
+                int correctNumbers = 0;
 
-
+                //Ask user for a starting and end number gets a range
                 Console.WriteLine("Enter a starting number");
                 startNum = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter a ending number");
                 endNum = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter six numbers within the range of " + startNum + " and " + endNum + " for a chance to win " + "$" + jackpot);
+                //Asks user to enter six numbers for a chance at the jackpot
+                Console.WriteLine("Enter six numbers within the range of " + startNum + " and " + endNum 
+                    + " for a chance to win $50,000 dollars!");
+
+
+                //A for loop that takes user input and enter the values into an array
                 for (int i = 0; i < userNum.Length; i++)
                 {
                     userNum[i] = int.Parse(Console.ReadLine());
@@ -39,15 +45,26 @@ namespace LuckyNumbers
                         userNum[i] = int.Parse(Console.ReadLine());
                     }
                 }
+                Console.WriteLine("\n");
 
+                //declares a variable r of class Random, assigning new instance of the class to the variable
                 Random r = new Random();
+
+
+                //declaring an integer array variable luckyNumbers, 
+                //assigning a new instance of the array of size 6 to variable
                 int[] luckyNumbers = new int[6];
-                int[] luckyNum = new int[6];
+                
+
+                //for loop that assigns each index of the luckNumbers array with random numbers
                 for (int j = 0; j < luckyNumbers.Length; j++)
                 {
                     luckyNumbers[j] = r.Next(startNum, endNum);
                 }
 
+
+                //nested for loop that checks for duplicate values within the luckyNumbers array, 
+                //then prints out Lucky Numbers
                 for (int k = 5; k > -1; k--)
                 {
                     for (int l = 0; l < k; l++)
@@ -57,9 +74,18 @@ namespace LuckyNumbers
                             luckyNumbers[l] = r.Next(startNum, endNum);
                         }
                     }
-                    Console.WriteLine("Lucky Numbers:" + luckyNumbers[k]);
+                    
                 }
 
+                for(int p = 0; p < luckyNumbers.Length; p++)
+                {
+                    Console.WriteLine("Lucky Number:" + luckyNumbers[p]);
+                }
+                Console.WriteLine("\n");
+
+
+                //nested for loop that compares each index value of the userNum array to each index of
+                //luckyNumbers array, counts matching values and assigns the amount to correctNumbers variable
                 for (int m = 0; m < luckyNumbers.Length; m++)
                 {
                     for (int n = 0; n < luckyNumbers.Length; n++)
@@ -71,27 +97,38 @@ namespace LuckyNumbers
                         }
                     }
                 }
-                Console.WriteLine("you guessed " + correctNumbers + " numbers correctly!");
 
-                winings = Earmings(jackpot, correctNumbers);
-                Console.WriteLine("Congrats!, You won $" + winings + "!");
+                
+                Console.WriteLine("you guessed " + correctNumbers + " numbers correctly! \n");
 
-
-                Console.WriteLine("would you like to play agian?: ");
-                answer = Console.ReadLine();
-
-                if (answer == "no")
+                if (correctNumbers == 6)
                 {
-                    Console.WriteLine("Thanks for Playing!");
+                    Console.WriteLine("JACKPOT!!!");
                 }
 
+
+                //assigns value returend from the Earnings method to the variable winings, prints winings
+                winings = Earmings(jackpot, correctNumbers);
+                Console.WriteLine("Congrats!, You just won $" + winings + "!\n");
+
+
+                Console.WriteLine("would you like to play agian?:");
+                answer = Console.ReadLine();
+
+
+                //
+                if (answer == "no")
+                {
+                    Console.WriteLine("\nThanks for Playing!");
+                }
+
+            //conditional that checks the user response to determine if the program continues 
             } while (answer != "no");
         }
 
+        //method that returns calculates the user winings based on the number of correct matches
         static double Earmings(double numOne, double numTwo)
         {
-
-
             double amount = numOne * (numTwo / 6);
             return amount;
         }
